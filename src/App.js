1922cec33827c76075e4952e182f5283a5aee326
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-23 21:16:05 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import './App.css'
 import { dummyContent } from './dummy-content'
+import cn from 'classname'
 
 class App extends Component {
+  state = { open: false }
+
   render() {
+    const open = this.state.open
     return (
-      <div className="App-root">
-        <div className="App-content">{dummyContent}</div>
-        <div className={'App-modal App-modal-open'}>
+      <div>
+        <div
+          className="App-content-wrapper"
+          onClick={() => this.setState({ open: true })}
+        >
+          <div className="App-content">{dummyContent}</div>
+        </div>
+        <div
+          className={cn('App-modal', {
+            'App-modal-open': open,
+          })}
+          onClick={() => this.setState({ open: false })}
+        >
           <div className="Modal-content">{dummyContent}</div>
         </div>
       </div>
